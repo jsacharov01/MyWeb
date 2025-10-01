c3bd43d9667870746de3657a312855c6977212ef
Jurij Sacharov <jurijsacharov@Jurijs-MacBook-Air.local>
2025-10-01 13:14:21 +0200

    Add project description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,14 +1,18 @@
 export const projects = [
 {
-name: "Projekt A",
-summary: "Digitalizace HR procesů, úspora 30 % času při onboardingu",
-},
-{
-name: "Projekt B",
-summary: "Implementace BI řešení, zvýšení kvality reportingu",
-},
-{
-name: "Projekt C",
-summary: "Migrace do cloudu, snížení provozních nákladů o 20 %",
-},
+    "name": "Analýza a implementace systému pro řízení kreditního rizika",
+    "summary": "Sběr a analýza požadavků, návrh řešení a koordinace implementace nového systému pro řízení kreditního rizika v nadnárodní bance. Zajištění spolupráce čtyř týmů ve čtyřech zemích."
+  },
+  {
+    "name": "Návrh a realizace interního auditního systému",
+    "summary": "Analýza potřeb uživatelů a návrh architektury auditního systému. Vedení implementace, která zajistila efektivnější práci s audity a jejich reporty."
+  },
+  {
+    "name": "Implementace DAC systému s využitím metodiky PRINCE2",
+    "summary": "Řízení projektu implementace DAC systému. Odpovědnost za komunikaci mezi vývojovým týmem a business zástupci, řízení rizik a koordinaci klíčových stakeholderů."
+  },
+  {
+    "name": "Vývoj systému pro spolupráci s klienty",
+    "summary": "Vedení agilního týmu při vývoji systému pro spolupráci s klienty. Zajištění správného pochopení požadavků a jejich realizace v rámci iterací a sprintů."
+  }
 ];
